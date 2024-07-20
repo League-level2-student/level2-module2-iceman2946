9e6cfde5cdc2086308dc41a87bf35349974f226d
league <league@adminisorsimac3.attlocal.net>
2024-07-20 16:32:21 -0700

    Animal

--- src/_04_animals_inheritance/Dog.java
+++ src/_04_animals_inheritance/Dog.java
@@ -1,5 +1,24 @@
 package _04_animals_inheritance;
 
 public class Dog {
-	
+	String name;
+	String color;
+	boolean isGirl;
+	Dog(String name, String color, boolean isGirl){
+		this.name=name;
+		this.color=color;
+		this.isGirl=isGirl;
+	}
+	public void printName() {
+		System.out.println("My name is "+name);
+	}
+	public void play() {
+		System.out.println("Playing");
+	}
+	public void eat() {
+		System.out.println("Eating");
+	}
+	public void sleep() {
+		System.out.println("Sleeping");
+	}
 }
