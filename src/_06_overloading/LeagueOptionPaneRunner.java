b10234e146d039af0c3328e7f220dba8f1d52334
league <league@adminisorsimac3.attlocal.net>
2024-07-27 16:30:10 -0700

    Second handle

--- src/_06_overloading/LeagueOptionPaneRunner.java
+++ src/_06_overloading/LeagueOptionPaneRunner.java
@@ -2,6 +2,6 @@ package _06_overloading;
 
 public class LeagueOptionPaneRunner {
 	public static void main(String[] args) {
-		
+		 new LeagueOptionPane().showMessageDialog("The League is the Best");
 	}
 }
